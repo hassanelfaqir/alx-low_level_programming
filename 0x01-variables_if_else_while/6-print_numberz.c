2bc0dac4212cade486be7e53c0c83ff706ef6813
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-17 09:59:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * descreption - all single digit numbers of base 10 starting from 0
+ * descreption - all single digit numbers of base 10 starting from 0.
  *
  * Return : Always 0 (Success)
  */
