dee8c380b022df280661643f7962aaaa7162cb95
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-17 09:25:43 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * descreption - digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int in = 0;
+
+	while (in < 10)
+	{
+		printf("%i", in);
+		in++;
+	}
+	printf("\n")
+
+	return (0);
+}
