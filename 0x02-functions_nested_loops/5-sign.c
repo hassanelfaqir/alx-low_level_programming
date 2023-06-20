a25c8a3fd9ac598086920c4b6d265a6e6e2790b4
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-20 09:58:10 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@
 
 int print_sign(int c)
 {
-	if (c > 0)
+       	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n < 0)
 	{
-		_putchar(48);
-		return (0);
-	}
-	else if (c < 0)
-	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
