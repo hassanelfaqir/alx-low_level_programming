0c780199d4c2dc4c0ec241d21d7fa6b7b91bb527
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-20 10:03:39 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Write a function that prints the sign of a number
  *
- * @c : check input of function
+ * @n : check input of function
  * Return:1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero(Success)
