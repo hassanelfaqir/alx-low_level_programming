7efcb40c23a5f36ef4b10af455ec2f5584e75eb1
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-20 08:26:50 -0700

    void print_alphabet_x10(void)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * prints the alphabet - prints 10 times the alphabet, in lowercae
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercae
  * 
  * return: void
  */
 
 void print_alphabet_x10(void)
+
 {
 	char i;
-	int s=0;
+	int s = 0;
 
 	while (s <= 9)
 	{
