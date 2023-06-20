e4a794e0e3eb3e08bd0aa8bb5c0a666942519ed8
El faqir hassan <hassanelfaqir99@gmail.com>
2023-06-20 10:34:02 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n : tayke the last digit of input fonction
+ *
+ * Return: last digit (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int lastdegit;
+
+	if (n < 0)
+		lastdegit = (-1) * (n % 10);
+	else
+		lastdegit = (n % 10);
+	_putchat(lastgegit + '0');
+	return (lastgegit);
+}
