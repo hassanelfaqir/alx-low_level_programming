a38e0329bc2d6b5cc73407a1a16a5e75bfb34ce7
El faqir hassan <hassanelfaqir99@gmail.com>
2023-07-31 16:35:02 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if (!tempt || !(temp->next))
+		if (!tempt || !(tempt->next))
 			return (-1);
 		tempt = tempt->next;
 		i++;
