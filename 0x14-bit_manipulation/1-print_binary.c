a6711b4021e06bc230088d58eda570e683a64276
El faqir hassan <hassanelfaqir99@gmail.com>
2023-08-03 16:04:22 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,14 +4,14 @@
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
-void print_binary(unsigned long int nb)
+void print_binary(unsigned long int n)
 {
 	int i, counte = 0;
 	unsigned long int current;
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = nb >> i;
+		current = n >> i;
 
 		if (current & 1)
 		{
