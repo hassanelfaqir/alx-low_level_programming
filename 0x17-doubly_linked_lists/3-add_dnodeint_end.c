dc14b4a1543e8b7d05c8d6ff7c6c8dc4d54ebb40
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2023-10-12 13:47:35 +0200

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (hdr != NULL)
 	{
 		while (hdr->next != NULL)
-			hdr = h->next;
+			hdr = hdr->next;
 		hdr->next = new_N;
 	}
 	else
